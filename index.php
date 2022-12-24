d2074fcecf4e55e9dc5195c2f60a66722830abef
arborin <nika.kobaidze@gmail.com>
2022-12-25 00:20:09 +0400

    [ADD] data types

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+
+echo "hello world";
+
+// constants
+define('NAME', "nika");
+const STATUS = 'draft';
+
+
+echo NAME;
+echo STATUS;
+
+// PRE DEFINED CONSTANTS
+echo PHP_VERSION;
+
+// variable variables
+$foo = 'bar';
+$$foo = 'baz';
+
+echo $bar;
+
+// DATA TYPES
+// 4 scalar types
+# bool
+# int
+# float
+
+// 4 compound types
+# array
+# object
+# callable
+# iterable
+
+// 2 Special types
+# resource
+# null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "\n";
